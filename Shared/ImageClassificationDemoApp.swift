a70c7ceef5356d62c110515ba06f44b4acd5fb7b
Jaleel Akbashev <adzhalil@gmail.com>
2021-02-08 16:17:13 +0500

    Init
    .gitignore is now working

---
+++ Shared/ImageClassificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationDemoApp.swift
+//  Shared
+//
+//  Created by Jaleel Akbashev on 08.02.21.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
